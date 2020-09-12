2ef1789e003457a0fae0a628469a984f3676c35e
twan-hub <adstith99@gmail.com>
2020-09-12 10:38:15 -0800

    search added

---
+++ src/main/java/com/grocerylist/app/controllers/SearchController.java
@@ -0,0 +1,53 @@
+package com.grocerylist.app.controllers;
+
+import com.grocerylist.app.models.GroceryList;
+import com.grocerylist.app.models.GroceryListData;
+import com.grocerylist.app.models.data.GroceryListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import java.util.HashMap;
+
+@Controller
+@RequestMapping("search")
+public class SearchController {
+
+
+    @Autowired
+    private GroceryListRepository groceryListRepository;
+
+    static HashMap<String, String> columnChoices = new HashMap<String,String>();
+
+    static {
+        columnChoices.put("all","All");
+        columnChoices.put("location", "Location");
+        columnChoices.put("name", "Name");
+        columnChoices.put("category", "Category");
+    }
+
+
+
+    @RequestMapping("")
+    public String search(Model model) {
+        model.addAttribute("columns", columnChoices);
+        return "search";
+    }
+
+    @PostMapping("results")
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm){
+        Iterable<GroceryList> jobs;
+        if (searchTerm.toLowerCase().equals("all") || searchTerm.equals("")){
+            jobs = groceryListRepository.findAll();
+        } else {
+            jobs = GroceryListData.findByColumnAndValue(searchType, searchTerm, groceryListRepository.findAll());
+        }
+        model.addAttribute("columns", columnChoices);
+        model.addAttribute("title", "Jobs with " + columnChoices.get(searchType) + ": " + searchTerm);
+        model.addAttribute("grocerylists", jobs);
+
+        return "search";
+    }
+}
---
+++ src/main/java/com/grocerylist/app/models/GroceryListData.java
@@ -0,0 +1,99 @@
+package com.grocerylist.app.models;
+
+import com.grocerylist.app.models.GroceryList;
+
+import java.util.ArrayList;
+
+// This is a change made in sandbox.
+
+/**
+ * Created by LaunchCode
+ */
+public class GroceryListData {
+
+
+    /**
+     * Returns the results of searching the Jobs data by field and search term.
+     *
+     * For example, searching for employer "Enterprise" will include results
+     * with "Enterprise Holdings, Inc".
+     *
+     * @param column Job field that should be searched.
+     * @param value Value of the field to search for.
+     * @param allList The list of jobs to search.
+     * @return List of all jobs matching the criteria.
+     */
+    public static ArrayList<GroceryList> findByColumnAndValue(String column, String value, Iterable<GroceryList> allList) {
+
+        ArrayList<GroceryList> results = new ArrayList<>();
+
+        if (value.toLowerCase().equals("all")){
+            return (ArrayList<GroceryList>) allList;
+        }
+
+        if (column.equals("all")){
+            results = findByValue(value, allList);
+            return results;
+        }
+        for (GroceryList groceryList : allList) {
+
+            String aValue = getFieldValue(groceryList, column);
+
+            if (aValue != null && aValue.toLowerCase().contains(value.toLowerCase())) {
+                results.add(groceryList);
+            }
+        }
+
+        return results;
+    }
+
+    public static String getFieldValue(GroceryList groceryList, String fieldName){
+        String theValue;
+        if (fieldName.equals("name")){
+            theValue = groceryList.getName();
+        } else if (fieldName.equals("location")){
+            theValue = groceryList.getLocation().toString();
+        } else if (fieldName.equals("category")){
+            theValue = groceryList.getCategory().toString();
+        }else {
+            theValue = groceryList.getItems().toString();
+        }
+
+        return theValue;
+    }
+
+    /**
+     * Search all Job fields for the given term.
+     *
+     * @param value The search term to look for.
+     * @param allList The list of jobs to search.
+     * @return      List of all jobs with at least one field containing the value.
+     */
+    public static ArrayList<GroceryList> findByValue(String value, Iterable<GroceryList> allList) {
+        String lower_val = value.toLowerCase();
+
+        ArrayList<GroceryList> results = new ArrayList<>();
+
+        for (GroceryList groceryList : allList) {
+
+            if (groceryList.getName().toLowerCase().contains(lower_val)) {
+                results.add(groceryList);
+            } else if (groceryList.getLocation().toString().toLowerCase().contains(lower_val)) {
+                results.add(groceryList);
+            } else if (groceryList.getCategory().toString().toLowerCase().contains(lower_val)) {
+                results.add(groceryList);
+            } else if (groceryList.getItems().toString().toLowerCase().contains(lower_val)) {
+            results.add(groceryList);
+            } else if (groceryList.toString().toLowerCase().contains(lower_val)) {
+                results.add(groceryList);
+            }
+
+        }
+
+        return results;
+    }
+
+
+}
+
+
